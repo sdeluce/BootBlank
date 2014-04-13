e7ba17943e4a963a163f5e7ee94cd0a95b6d3396
boluge <boluge@gmail.com>
2014-04-13 14:31:32 +0200

    up

--- footer.php
+++ footer.php
@@ -2,14 +2,16 @@
 			<!-- /Container -->
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
-
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'bootblank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//bootblank.com" title="HTML5 Blank">BootBlank</a>.
-				</p>
-				<!-- /copyright -->
-
+				<div class="container">
+					<div class="col-md-12">
+						<!-- copyright -->
+						<p class="copyright">
+							&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'bootblank'); ?>
+							<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//bootblank.com" title="HTML5 Blank">BootBlank</a>.
+						</p>
+						<!-- /copyright -->
+					</div>
+				</div>
 			</footer>
 			<!-- /footer -->
 
--- header.php
+++ header.php
@@ -30,20 +30,22 @@
 			<!-- header -->
 			<header class="header clear" role="banner">
 				<div class="container">
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
+					<div class="col-md-12">
+						<!-- logo -->
+						<div class="logo">
+							<a href="<?php echo home_url(); ?>">
+								<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+								<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+							</a>
+						</div>
+						<!-- /logo -->
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php bootblank_nav(); ?>
-					</nav>
-					<!-- /nav -->
+						<!-- nav -->
+						<nav class="nav" role="navigation">
+							<?php bootblank_nav(); ?>
+						</nav>
+						<!-- /nav -->
+					</div>
 				</div>
 			</header>
 			<!-- /header -->
