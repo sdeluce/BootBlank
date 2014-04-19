f9af8bf031eee6fa1f5c7675bef21f249462d0fe
boluge <boluge@gmail.com>
2014-04-19 12:41:26 +0200

    up

--- options.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$main_col = 'col-md-9';
-	$sidebar_col = 'col-md-3';
-?>
--- template-demo.php
+++ template-demo.php
@@ -1,45 +1,23 @@
-<?php /* Template Name: Demo Page Template */ get_header(); ?>
+<?php
+/* Template Name: Demo Page Template */
+get_header(); ?>
 
-	<main role="main">
+<?php get_sidebar(); ?>
+
+	<main class="<?php bootblank_main_class(); ?>" role="main">
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?></h1>
+			<h1><?php _e( 'Latest Posts', 'bootblank' ); ?></h1>
 
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<?php get_template_part('loop'); ?>
 
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'bootblank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
+			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php get_sidebar('right'); ?>
 
 <?php get_footer(); ?>
