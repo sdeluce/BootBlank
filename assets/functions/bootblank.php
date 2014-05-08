44fbf9e1158d5bc36377869eb6e9bbccecf78547
boluge <boluge@gmail.com>
2014-05-08 14:19:07 +0200

    update adminbar perso

--- assets/functions/bootblank.php
+++ assets/functions/bootblank.php
@@ -1,47 +1,47 @@
 <?php
 /*------------------------------------*\
-    Sidebar Class Bootstrap
+	Sidebar Class Bootstrap
 \*------------------------------------*/
 function bootblank_main_class() {
-    if ( is_active_sidebar('widget-area-1') && is_active_sidebar('widget-area-2') ) {
-        echo 'col-sm-6';
-    } else if ( is_active_sidebar('widget-area-1') || is_active_sidebar('widget-area-2') ) {
-        echo 'col-sm-9';
-    } else {
-        // Classes on full width pages
-        echo 'col-sm-12';
-    }
+	if ( is_active_sidebar('widget-area-1') && is_active_sidebar('widget-area-2') ) {
+		echo 'col-sm-6';
+	} else if ( is_active_sidebar('widget-area-1') || is_active_sidebar('widget-area-2') ) {
+		echo 'col-sm-9';
+	} else {
+		// Classes on full width pages
+		echo 'col-sm-12';
+	}
 }
 function bootblank_sidebar_class() {
-    echo 'col-sm-3';
+	echo 'col-sm-3';
 }
 
 /*------------------------------------*\
-    External Modules/Files
+	External Modules/Files
 \*------------------------------------*/
 
 // Login override CSS  --Front--
 function bootblank_login_css()  {
-    echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('template_directory') . '/css/login.css?v=1.0.0" />';
+	echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('template_directory') . '/css/login.css?v=1.0.0" />';
 }
 add_action('login_head', 'bootblank_login_css'); // Add Override Login Css
 
 function bootblank_dashboard_help() {
-    echo '<p style="text-align:center;"><img src="'.get_template_directory_uri().'/img/logo.png"/></p>';
+	echo '<p style="text-align:center;"><img src="'.get_template_directory_uri().'/img/logo.png"/></p>';
 }
 //Deletes empty classes and removes the sub menu class --front--
 function change_submenu_class($menu) {
-    $menu = preg_replace('/ class="sub-menu"/','/ class="dropdown" /',$menu);
-    return $menu;
+	$menu = preg_replace('/ class="sub-menu"/','/ class="dropdown" /',$menu);
+	return $menu;
 }
 add_filter ('wp_nav_menu','change_submenu_class'); // Add class menu
 
 // Encapsulage des videos
 function standard_wrap_embeds( $html, $url, $args ) {
-    if( 'video' == get_post_format( get_the_ID() ) ) {
-        $html = '<div class="video-wrapper">' . $html . '</div>';
-    } // end if
-    return $html;
+	if( 'video' == get_post_format( get_the_ID() ) ) {
+		$html = '<div class="video-wrapper">' . $html . '</div>';
+	} // end if
+	return $html;
 } // end standard_wrap_embebds
 add_filter( 'embed_oembed_html', 'standard_wrap_embeds', 10, 3 ) ; // Video responsive
 
@@ -49,9 +49,9 @@ add_filter( 'embed_oembed_html', 'standard_wrap_embeds', 10, 3 ) ; // Video resp
 $post_formats = array( 'aside', 'chat', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio' );
 
 function favicons() {
-    $icosource = file_get_contents( get_template_directory_uri().'/assets/icons/icons.html' , "r");
-    $head_ico = str_replace("%url%",  get_template_directory_uri(), $icosource);
-    echo $head_ico;
+	$icosource = file_get_contents( get_template_directory_uri().'/assets/icons/icons.html' , "r");
+	$head_ico = str_replace("%url%",  get_template_directory_uri(), $icosource);
+	echo $head_ico;
 }
 add_action('wp_head', 'favicons');
 
@@ -59,22 +59,23 @@ add_action('wp_head', 'favicons');
 add_action( 'admin_enqueue_scripts', 'remove_jquery' );
 function remove_jquery()
 {
-    wp_deregister_script('jquery');
+	wp_deregister_script('jquery');
 }
 
 // Obscure login screen error messages
 function bootblank_login_obscure(){
-    return '<strong>Sorry</strong>: Think you have gone wrong somwhere!';
+	return '<strong>Sorry</strong>: Think you have gone wrong somwhere!';
 }
 
 // Posts Formats
 $post_formats = array( 'aside', 'chat', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio' );
+add_theme_support('post-formats', $post_formats); // Ajout de Post Format
 
 // Add Dashicons in the theme
 function bootblank_dashicons() {
-    wp_enqueue_style('dashicons');
+	wp_enqueue_style('dashicons');
 }
-// add_action('wp_enqueue_scripts', 'bootblank_dashicons'); // Utilisation de Dashicon WP 3.8
+// add_action('wp_enqueue_scripts', 'bootblank_dashicons'); // Utilisation de Dashicon WP 3.8 en front
 
 // Remove Actions
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
@@ -82,24 +83,26 @@ remove_action('wp_head', 'wp_generator'); // Remove Wordpress version
 
 // Add Filter
 add_filter( 'login_errors', 'bootblank_login_obscure' ); // Remove login error
-// add_filter('jpeg_quality', function($arg){return 80;}); // Compression des images à 80% au lieu de 90%
-add_filter( 'jpeg_quality', create_function( '', 'return 80;' ) ); // Idem php < 5.3
+add_filter('jpeg_quality', function($arg){return 80;}); // Compression des images à 80% au lieu de 90%
+// add_filter( 'jpeg_quality', create_function( '', 'return 80;' ) ); // Idem php < 5.3
 
 // Remove Filters
 remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
 
 /*------------------------------------*\
-    Move admin bar
+	Move admin bar
 \*------------------------------------*/
 function fb_move_admin_bar() {
-    $adminbar = file_get_contents( get_template_directory_uri().'/css/adminbar.css' , "r");
-    echo '<style type="text/css">'.$adminbar.'</style>';
+	$adminbar = file_get_contents( get_template_directory_uri().'/css/adminbar.css' , "r");
+	if ( is_user_logged_in() ) {
+		echo '<style type="text/css">'.$adminbar.'</style>';
+	}
 }
 // en front-end
 add_action( 'wp_head', 'fb_move_admin_bar' );
 
 /*------------------------------------*\
-    External Modules/Files
+	External Modules/Files
 \*------------------------------------*/
 
 // Load any external files you have here
