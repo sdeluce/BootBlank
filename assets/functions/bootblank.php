3f35907d45f87e6cd43bcf2d7a27fa6ace6995d0
boluge <boluge@gmail.com>
2014-05-08 14:11:22 +0200

    divide Functions file

--- assets/functions/bootblank.php
+++ assets/functions/bootblank.php
@@ -15,35 +15,6 @@ function bootblank_main_class() {
 function bootblank_sidebar_class() {
     echo 'col-sm-3';
 }
-/*------------------------------------*\
-    Colonnnes personnalisées
-\*------------------------------------*/
-function custom_slider_columns_type($columns) {
-
-    $columns['title'] = 'Titre du slider';
-    $columns['date'] = 'Créé le';
-
-    $columns = array_slice($columns, 0, 1) + array( 'thumbnail' =>'Miniature') + array_slice($columns, 1, count($columns)-1, true );
-
-    return $columns;
-}
-add_filter('manage_edit-slider_columns', 'custom_slider_columns_type');
-
-function custom_slider_columns_content($column) {
-    global $post;
-
-    switch ($column) {
-        case 'thumbnail':
-            the_post_thumbnail( 'vignette' );
-            break;
-        case 'publication_date':
-            $args = array( 'post_type' => 'bootblank', 'numberposts' => -1, 'meta_query' => array( array( 'key' => '_book_info_author', 'value' => $post->ID ) ) );
-            $books = get_posts($args);
-            echo count($books);
-            break;
-    }
-}
-add_action('manage_slider_posts_custom_column', 'custom_slider_columns_content');
 
 /*------------------------------------*\
     External Modules/Files
@@ -53,7 +24,7 @@ add_action('manage_slider_posts_custom_column', 'custom_slider_columns_content')
 function bootblank_login_css()  {
     echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('template_directory') . '/css/login.css?v=1.0.0" />';
 }
-add_action('login_head', 'foundation_login_css'); // Add Override Login Css
+add_action('login_head', 'bootblank_login_css'); // Add Override Login Css
 
 function bootblank_dashboard_help() {
     echo '<p style="text-align:center;"><img src="'.get_template_directory_uri().'/img/logo.png"/></p>';
@@ -64,41 +35,6 @@ function change_submenu_class($menu) {
     return $menu;
 }
 add_filter ('wp_nav_menu','change_submenu_class'); // Add class menu
-
-// Préchargement des pages --front--
-function bootblank_prefetch() {
-    if ( is_single() ) {  ?>
-        <!-- Préchargement de la page d\'accueil -->
-        <link rel="prefetch" href="<?php echo home_url(); ?>" />
-        <link rel="prerender" href="<?php echo home_url(); ?>" />
-
-        <!-- Préchargement de l\'article suivant -->
-        <link rel="prefetch" href="<?php echo get_permalink( get_adjacent_post(false,'',true) ); ?>" />
-        <link rel="prerender" href="<?php echo get_permalink( get_adjacent_post(false,'',true) ); ?>" />
-   <?php
-   }
-}
-add_action('wp_head', 'bootblank_prefetch'); // Optimisation
-
-/** Début Minification des fichiers HTML **/
-function bootblank_html_minify_start() {
-    ob_start( 'bootblank_html_minyfy_finish' );
-}
-
-function bootblank_html_minyfy_finish( $html ) {
-
-    // Suppression des commentaires HTML, sauf les commentaires conditionnels pour IE
-    $html = preg_replace('/<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->/s', '', $html);
-
-    // Suppression des espaces vides
-    $html = str_replace(array("\r\n", "\r", "\n", "\t"), '', $html);
-    while ( stristr($html, '  '))
-        $html = str_replace('  ', ' ', $html);
-
-    return $html;
-}
-add_action('get_header', 'bootblank_html_minify_start');
-/** Fin Minification des fichiers HTML **/
 
 // Encapsulage des videos
 function standard_wrap_embeds( $html, $url, $args ) {
@@ -153,48 +89,6 @@ add_filter( 'jpeg_quality', create_function( '', 'return 80;' ) ); // Idem php <
 remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
 
 /*------------------------------------*\
-    Add Color to editor Functions
-\*------------------------------------*/
-function my_mce4_options( $init ) {
-$default_colours = '
-    "000000", "Black",        "993300", "Burnt orange", "333300", "Dark olive",   "003300", "Dark green",   "003366", "Dark azure",   "000080", "Navy Blue",      "333399", "Indigo",       "333333", "Very dark gray",
-    "800000", "Maroon",       "FF6600", "Orange",       "808000", "Olive",        "008000", "Green",        "008080", "Teal",         "0000FF", "Blue",           "666699", "Grayish blue", "808080", "Gray",
-    "FF0000", "Red",          "FF9900", "Amber",        "99CC00", "Yellow green", "339966", "Sea green",    "33CCCC", "Turquoise",    "3366FF", "Royal blue",     "800080", "Purple",       "999999", "Medium gray",
-    "FF00FF", "Magenta",      "FFCC00", "Gold",         "FFFF00", "Yellow",       "00FF00", "Lime",         "00FFFF", "Aqua",         "00CCFF", "Sky blue",       "993366", "Brown",        "C0C0C0", "Silver",
-    "FF99CC", "Pink",         "FFCC99", "Peach",        "FFFF99", "Light yellow", "CCFFCC", "Pale green",   "CCFFFF", "Pale cyan",    "99CCFF", "Light sky blue", "CC99FF", "Plum",         "FFFFFF", "White"
-';
-$custom_colours = '
-    "e14d43", "Color 1 Name", "d83131", "Color 2 Name", "ed1c24", "Color 3 Name", "f99b1c", "Color 4 Name", "50b848", "Color 5 Name", "00a859", "Color 6 Name",   "00aae7", "Color 7 Name", "282828", "Color 8 Name"
-';
-$init['textcolor_map'] = '['.$custom_colours.','.$default_colours.']';
-return $init;
-}
-add_filter('tiny_mce_before_init', 'my_mce4_options');
-
-/*------------------------------------*\
-    Defer js
-\*------------------------------------*/
-function defer_parsing_of_js ( $url ) {
-    if ( FALSE === strpos( $url, '.js' ) ) return $url;
-    if ( strpos( $url, 'jquery.js' ) ) return $url;
-    return "$url' defer";
-}
-add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
-
-automatic_feed_links(false);
-
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
-// remove_action('wp_head', 'wp_dlmp_l10n_style' );
-// remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link');
-
-/*------------------------------------*\
-    BackCompat wp 3.8
-\*------------------------------------*/
-if ( version_compare( $GLOBALS['wp_version'], '3.8', '<' ) ) {
-    require get_template_directory() . '/assets/functions/back-compat.php';
-}
-/*------------------------------------*\
     Move admin bar
 \*------------------------------------*/
 function fb_move_admin_bar() {
@@ -209,7 +103,8 @@ add_action( 'wp_head', 'fb_move_admin_bar' );
 \*------------------------------------*/
 
 // Load any external files you have here
+require_once ('optim.php');
+require_once ('custompost.php');
 // require_once ('widget.php');
-// require_once ('custompost.php');
 // require_once ('woosupport.php');
 ?>
--- assets/functions/custompost.php
+++ assets/functions/custompost.php
@@ -6,8 +6,8 @@ function add_menu_icons_styles(){
 ?>
 
 <style>
-#adminmenu #menu-posts-bootblank div.wp-menu-image:before {
-    content: "\f328";
+#adminmenu #menu-posts-slider div.wp-menu-image:before {
+    content: "\f115";
 }
 </style>
 
@@ -20,26 +20,26 @@ function create_post_type_bootblank()
 {
     register_taxonomy_for_object_type('category', 'bootblank'); // Register Taxonomies for Category
     register_taxonomy_for_object_type('post_tag', 'bootblank');
-    register_post_type('bootblank', // Register Custom Post Type
+    register_post_type('slider', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('BootBlank', 'bootblank'), // Rename these to suit
-            'singular_name' => __('BootBlank', 'bootblank'),
-            'add_new' => __('Add New', 'bootblank'),
-            'add_new_item' => __('Add New BootBlank Custom Post', 'bootblank'),
-            'edit' => __('Edit', 'bootblank'),
-            'edit_item' => __('Edit BootBlank Custom Post', 'bootblank'),
-            'new_item' => __('New BootBlank Custom Post', 'bootblank'),
-            'view' => __('View BootBlank Custom Post', 'bootblank'),
-            'view_item' => __('View BootBlank Custom Post', 'bootblank'),
-            'search_items' => __('Search BootBlank Custom Post', 'bootblank'),
-            'not_found' => __('No BootBlank Custom Posts found', 'bootblank'),
-            'not_found_in_trash' => __('No BootBlank Custom Posts found in Trash', 'bootblank')
+            'name' => __('Sliders', 'bootblank'), // Rename these to suit
+            'singular_name' => __('Slider', 'bootblank'),
+            'add_new' => __('Ajouter un slider', 'bootblank'),
+            'add_new_item' => __('Ajouter un slider', 'bootblank'),
+            'edit' => __('Editer un slider', 'bootblank'),
+            'edit_item' => __('Editer un slider', 'bootblank'),
+            'new_item' => __('Nouveau slider', 'bootblank'),
+            'view' => __('Voir le slider', 'bootblank'),
+            'view_item' => __('Voir le slider', 'bootblank'),
+            'search_items' => __('Chercher un slider', 'bootblank'),
+            'not_found' => __('Aucun slider trouvé', 'bootblank'),
+            'not_found_in_trash' => __('Pas de slider dans la corbeille', 'bootblank')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
         'has_archive' => true,
-        'menu_position'=>32,
+        'menu_position'=>55,
         'supports' => array(
             'title',
             'editor',
@@ -48,13 +48,13 @@ function create_post_type_bootblank()
         ), // Go to Dashboard Custom BootBlank post for supports
         'can_export' => true, // Allows export in Tools > Export
         'taxonomies' => array(
-            'post_tag',
-            'category'
+         'post_tag',
+         'category'
         ) // Add Category and Post Tags support
     ));
 }
-
 add_action('init', 'create_post_type_bootblank'); // Add our BootBlank Custom Post Type
+
 /*------------------------------------*\
     Create menu separator
 \*------------------------------------*/
@@ -80,176 +80,30 @@ function add_admin_menu_separator($position) {
     Adds menu separator
 \*------------------------------------*/
 function admin_menu_separator() {
+
     // Adds custom separator after comments
-    add_admin_menu_separator(31);
-}
-
-/*------------------------------------*\
-    Champs personnalisées
-\*------------------------------------*/
-global $myposttype;
-global $myCustomTypeOptions;
-$myposttype='bootblank'; //id de mon custom post
-
-$myCustomTypeOptions = array (
-    array(
-        'name' => 'Mon champ text',
-        'desc' => 'Description 1',
-        'id' => $myposttype.'_champtexte',
-        'type' => 'text',
-        'std' => ''),
-     array(
-        'name' => 'Mon textarea',
-        'desc' => 'Description 2',
-        'id' => $myposttype.'_textarea',
-        'type' => 'textarea',
-        'std' => 'Youpi'),
-    array(
-        'name' => 'Mon select',
-        'desc' => '',
-        'id' => $myposttype.'_choix',
-        'type' => 'select',
-        'options' => array('choix 1','choix 2','choix 3'),
-        'std' => 'choix 2'),
-    array(
-        'name' => 'Mon checkbox',
-        'desc' => 'Case Ã  cocher',
-        'id' => $myposttype.'_case',
-        'type' => 'checkbox',
-        'std' => '1')
-);
-
-/************** Ã  la fin du formulaire, ajout des champs dÃ©finis dans $myCustomTypeOptions ***********/
-add_action('edit_form_advanced', 'moncustomposttype_form');
-add_action('save_post', 'moncustomposttype_save');
-function moncustomposttype_form(){
-
-    global $myposttype;
-    global $myCustomTypeOptions;
-
-    if((isset($_GET['post_type'])) and ($_GET['post_type']==$myposttype)) /* formulaire d'ajout */
-    {
-        echo '<div class="meta-box-sortables ui-sortable">';
-        /* formulaire vide (ou avec les valeurs par dÃ©faut) */
-        foreach ($myCustomTypeOptions as $o)
-        {
-            echo '<div class="postbox">';
-            echo '<h3 class="hndle"><span>'.$o['name'].'</span></h3><div class="inside"><label class="screen-reader-text" for="'.$o['id'].'">'.$o['name'].'</label>';
-
-            //fonction pour afficher le bon html en fonction du type, dÃ©finie ci-aprÃ¨s
-            echo get_champ($o,$o['std']);
-
-            if($o['desc']!='')
-                echo '<p>'.$o['desc'].'</p>';
-
-            echo '</div></div>';
-        }
-        echo '</div>';
-
-    }
-    else{
-        if(!isset($_GET['post_type']))
-        {
-            if(isset($_GET['post']))
-            {
-                if(get_post_type($_GET['post'])==$myposttype) /* formulaire de modification */
-                {
-                    $id=$_GET['post'];
-
-                    /* formulaire prÃ©rempli avec les valeurs prÃ©-existantes */
-                    echo '<div class="meta-box-sortables ui-sortable">';
-                    foreach ($myCustomTypeOptions as $o)
-                    {
-                        echo '<div class="postbox">';
-                        echo '<h3 class="hndle"><span>'.$o['name'].'</span></h3><div class="inside"><label class="screen-reader-text" for="'.$o['id'].'">'.$o['name'].'</label>';
-
-                        echo get_champ($o,get_post_meta($id, $o['id'], true));
-
-                        if($o['desc']!='')
-                            echo '<p>'.$o['desc'].'</p>';
-
-                        echo '</div></div>';
-                    }
-                    echo '</div>';
-                }
-            }
-        }
-    }
-}
-
-function get_champ($o,$val)
-{
-    switch ($o['type'])
-    {
-        case 'textarea':
-            echo '  <textarea rows="2" cols="40" name="'.$o['id'].'" id="'.$o['id'].'">'.$val.'</textarea>';
-            break;
-        case 'text':
-            echo '  <input type="text" style="width:100%;" name="'.$o['id'].'" id="'.$o['id'].'" value="'.$val.'"/>';
-            break;
-        case 'checkbox':
-            echo '<input type="checkbox" name="'.$o['id'].'" id="'.$o['id'].'" value="1" ';
-            if($val==1)
-                echo 'checked="checked';
-            echo '/><label for="'.$o['id'].'">'.$o['name'].'</label>';
-            break;
-        case 'select':
-            echo '  <select name="'.$o['id'].'" style="width:100%;" id="'.$o['id'].'"><option value="">-</option>';
-            foreach($o['options'] as $opt)
-            {
-                echo '<option value="'.$opt.'"';
-                if($opt==$val)
-                    echo 'selected="selected"';
-                echo '>'.$opt.'</option>';
-            }
-            echo'</select>';
-            break;
-    }
-
-}
-/***************** Lors de la sauvegarde, ajout/modification d'un custom field par champs de $myCustomTypeOptions ***********/
-function moncustomposttype_save(){
-
-    global $myposttype;
-    global $myCustomTypeOptions;
-
-    if(isset($_POST['post_ID']))
-    {
-        $id=$_REQUEST['post_ID'];
-        if(get_post_type($id)==$myposttype) /* si on Ã©dite bien un post du type $myposttype */
-        {
-            foreach ($myCustomTypeOptions as $o)
-            {
-                if(isset($_POST[$o['id']])) {
-                    update_post_meta($id, $o['id'], $_POST[$o['id']]);
-                }
-                elseif($o['type']=='checkbox') {
-                    update_post_meta($id, $o['id'], 0);
-                }
-            }
-        }
-    }
+    add_admin_menu_separator(53);
 }
 /*------------------------------------*\
     Colonnnes personnalisées
 \*------------------------------------*/
-function custom_bootblank_columns_type($columns) {
+function custom_slider_columns_type($columns) {
 
-    $columns['title'] = 'Titre du BootBlank';
+    $columns['title'] = 'Titre du slider';
     $columns['date'] = 'Créé le';
 
     $columns = array_slice($columns, 0, 1) + array( 'thumbnail' =>'Miniature') + array_slice($columns, 1, count($columns)-1, true );
 
     return $columns;
 }
-add_filter('manage_edit-bootblank_columns', 'custom_bootblank_columns_type');
+add_filter('manage_edit-slider_columns', 'custom_slider_columns_type');
 
-function custom_bootblank_columns_content($column) {
+function custom_slider_columns_content($column) {
     global $post;
 
     switch ($column) {
         case 'thumbnail':
-            the_post_thumbnail( 'small' );
+            the_post_thumbnail( 'vignette' );
             break;
         case 'publication_date':
             $args = array( 'post_type' => 'bootblank', 'numberposts' => -1, 'meta_query' => array( array( 'key' => '_book_info_author', 'value' => $post->ID ) ) );
@@ -258,6 +112,25 @@ function custom_bootblank_columns_content($column) {
             break;
     }
 }
-add_action('manage_bootblank_posts_custom_column', 'custom_bootblank_columns_content');
+add_action('manage_slider_posts_custom_column', 'custom_slider_columns_content');
+
+/*------------------------------------*\
+    Add Color to editor Functions
+\*------------------------------------*/
+function my_mce4_options( $init ) {
+$default_colours = '
+    "000000", "Black",        "993300", "Burnt orange", "333300", "Dark olive",   "003300", "Dark green",   "003366", "Dark azure",   "000080", "Navy Blue",      "333399", "Indigo",       "333333", "Very dark gray",
+    "800000", "Maroon",       "FF6600", "Orange",       "808000", "Olive",        "008000", "Green",        "008080", "Teal",         "0000FF", "Blue",           "666699", "Grayish blue", "808080", "Gray",
+    "FF0000", "Red",          "FF9900", "Amber",        "99CC00", "Yellow green", "339966", "Sea green",    "33CCCC", "Turquoise",    "3366FF", "Royal blue",     "800080", "Purple",       "999999", "Medium gray",
+    "FF00FF", "Magenta",      "FFCC00", "Gold",         "FFFF00", "Yellow",       "00FF00", "Lime",         "00FFFF", "Aqua",         "00CCFF", "Sky blue",       "993366", "Brown",        "C0C0C0", "Silver",
+    "FF99CC", "Pink",         "FFCC99", "Peach",        "FFFF99", "Light yellow", "CCFFCC", "Pale green",   "CCFFFF", "Pale cyan",    "99CCFF", "Light sky blue", "CC99FF", "Plum",         "FFFFFF", "White"
+';
+$custom_colours = '
+    "e14d43", "Color 1 Name", "d83131", "Color 2 Name", "ed1c24", "Color 3 Name", "f99b1c", "Color 4 Name", "50b848", "Color 5 Name", "00a859", "Color 6 Name",   "00aae7", "Color 7 Name", "282828", "Color 8 Name"
+';
+$init['textcolor_map'] = '['.$custom_colours.','.$default_colours.']';
+return $init;
+}
+add_filter('tiny_mce_before_init', 'my_mce4_options');
 
 ?>
