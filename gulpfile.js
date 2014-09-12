df9bbec8cbadf9ca257cfda613db1517eebb1e98
Stephane Deluce <boluge@ymail.com>
2014-09-12 23:57:48 +0200

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,35 @@
 var 	gulp = require('gulp'),
+ 	watch = require('gulp-watch'),
+	plumber = require('gulp-plumber'),
 	livereload = require('gulp-livereload'),
 	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
 	clean = require('gulp-clean'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	imagemin = require('gulp-imagemin'),
+	svgmin = require('gulp-svgmin');
 	pngcrush = require('imagemin-pngcrush'),
-	svg2png = require('gulp-svg2png');
+	svg2png = require('gulp-svg2png'),
+	filter      = require('gulp-filter'),
+	browserSync = require("browser-sync"),
+	notify = require("gulp-notify");
 
 
+/*	Javascript
+*******************************************/
 gulp.task('coffee', function() {
 	return gulp.src('assets/coffee/*.coffee')
+		.pipe(plumber())
 		.pipe(coffee({
 			bare: true
 		}))
-		.pipe(gulp.dest('assets/js'));
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('assets/js'))
+		.pipe(notify("Coffee: <%= file.relative %>!"));
 
 });
-
-gulp.task('sass', function() {
-
-	return gulp.src('assets/sass/*.sass')
-		.pipe(sass({
-			lineNumbers: true,
-			sourcemapPath: 'css/sass',
-			style: 'nested'
-		}))
-		.pipe(gulp.dest('css'));
-
-});
-
-gulp.task('svg2png', function () {
-	return gulp.src('assets/svg/*.svg')
-		.pipe(svg2png())
-		.pipe(gulp.dest('assets/img/svg'));
-});
-
-gulp.task('img', ['svg2png'], function () {
-
-	return gulp.src('assets/img/*')
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngcrush()],
-			number: 4
-		}))
-		.pipe(gulp.dest('img'));
-
-	return gulp.src('assets/img/icons/*')
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngcrush()],
-			number: 4
-		}))
-		.pipe(gulp.dest('img/icons'))
-
-});
-
 gulp.task('js', ['coffee'], function() {
 	return gulp.src(
 		'assets/ext_libs/jquery/dist/jquery.js',
@@ -69,50 +40,112 @@ gulp.task('js', ['coffee'], function() {
 		'assets/js/cookie.js',
 		'assets/js/script.js'
 	)
+		.pipe(plumber())
 		.pipe(concat('script.min.js'))
-		.pipe(gulp.dest('assets/js/'));
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('assets/js/'))
+		.pipe(notify("JS: <%= file.relative %>!"));
 });
 gulp.task('uglify', ['js'], function() {
 	gulp.src('assets/js/script.min.js')
+		.pipe(plumber())
 		.pipe(uglify())
-		.pipe(gulp.dest('js'));
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('js'))
+		.pipe(notify("Js min: <%= file.relative %>!"));
 });
 
-gulp.task('dev', ['uglify', 'sass', 'img'], function() {
+/*	Style
+*******************************************/
+gulp.task('sass', function() {
+	gulp.src('assets/sass/*.sass')
+		.pipe(plumber())
+		.pipe(sass({
+			lineNumbers: true,
+			sourcemapPath: 'css/sass',
+			style: 'nested'
+		}))
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('css'));
+
+});
+gulp.task('css', ['sass'], function() {
+	return gulp.src('assets/css/*.css')
+		.pipe(plumber())
+		.pipe(autoprefixer())
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('css'));
+});
+
+/*	Images
+*******************************************/
+gulp.task('svg2png', function () {
+	return gulp.src('assets/svg/*.svg')
+		.pipe(svg2png())
+		.pipe(gulp.dest('assets/img'));
+});
+
+gulp.task('svgmin', function () {
+	return gulp.src('assets/svg/*.svg')
+		.pipe(svgmin())
+		.pipe(gulp.dest('assets/img'));
+});
+
+gulp.task('img', ['svgmin','svg2png'], function () {
+
+	return gulp.src('assets/img/**.*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngcrush()],
+			number: 4
+		}))
+		.pipe(gulp.dest('img'));
 
 })
 
+/*	Other
+*******************************************/
+gulp.task('clean', function() {
+	return gulp.src("css/*.**")
+		.pipe(plumber())
+		.pipe(clean({
+			force: true
+		}))
+		.pipe(plumber().stop())
+})
+
+/*	Watch
+*******************************************/
 gulp.task('watch', function() {
-	watch('assets/sass/*.sass', function(files) {
-		return files.pipe(gulp.dest('css/'));
-	});
-})
-
-gulp.task('default', ['dev'], function() {
 	var server = livereload();
 
+	watch('assets/sass/*.sass', function(files) {
+		return files.pipe(sass())
+		.pipe(gulp.dest('css/'))
+		.pipe(notify("Le fichier <%= file.relative %> à été généré !"))
+	});
 	gulp.watch('assets/coffee/*.coffee', ['coffee', 'uglify']);
 	gulp.watch('assets/js/*.js', ['uglify']);
-	gulp.watch('assets/sass/*.sass', ['sass']);
 
 	gulp.watch(['*.php', 'css/*.css', 'js/*.js']).on('change', function(event){
 		server.changed(event.path);
 	})
 })
 
-gulp.task('clean', function() {
-	return gulp.src("css/*.**")
-		.pipe(clean({
-			force: true
-		}));
-});
+////////////////////////////////////////////////////////////////////////////////
+//////////
+//////////     TACHES
+//////////
+////////////////////////////////////////////////////////////////////////////////
+gulp.task('dev', ['img', 'uglify', 'sass'], function() {
+
+})
+
+gulp.task('default', ['dev', 'watch'], function() {
+
+})
 
 gulp.task('build', ['clean', 'js', 'img'], function() {
-	gulp.src('assets/sass/*.sass')
-		.pipe(sass({
-			lineNumbers: true,
-			sourcemapPath: 'css/sass',
-			style: 'nested'
-		}))
-		.pipe(gulp.dest('css'));
+
 });
